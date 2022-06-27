3f266a23d287107a35135407b62a5c0476eab323
Justin Liman <justinliman97@gmail.com>
2022-06-27 04:44:23 -0700

    add chokidar module to watch files

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('hi');
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+
+
+const start = debounce(() => {
+    console.log('STARTING USERS PROGRAM');
+}, 100);
+
+chokidar.watch('.')
+    .on('add', () => console.log('FILE ADDED'))
+    .on('change', () => console.log('FILE CHANGED'))
+    .on('unlink', () => console.log('FILE UNLINKED'));
